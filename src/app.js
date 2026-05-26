4ae6e06a64c2461603acf1371ec935c6f2eca1a7
leoh82 <leoh82@devftleoh.devftleoh>
2026-05-26 12:00:23 -0300

    estructura inicial backend profesional

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "API funcionando correctamente"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`servidor corriendo en puerto ${PORT}`);
+});
