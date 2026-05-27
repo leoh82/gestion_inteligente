d5f521133a380649082967ea3edddf220fe1de84
leoh82 <leoh82@devftleoh.devftleoh>
2026-05-27 08:39:14 -0300

    estructura inicial backend profesional

--- src/app.js
+++ src/app.js
@@ -17,6 +17,12 @@ app.get("/", (req, res) => {
     });
 });
 
+app.get("/health", (req, res) =>{
+    res.json({
+        status:"ok"
+    });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
